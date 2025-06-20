06e9ce21b8280a7409c4e5d420da8d877ac0c3e6
crazybotcreation <crazyechooo@gmail.com>
2025-06-20 13:33:50 +0530

    Create ping.js

---
+++ src/commands/ping.js
@@ -0,0 +1,10 @@
+// src/commands/ping.js
+export default {
+  data: {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  async execute(interaction) {
+    await interaction.reply('🏓 Pong!');
+  },
+}
