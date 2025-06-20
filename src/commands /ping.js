fb1bc9271435e03d6da983bfaccfc950b653d2d5
crazybotcreation <crazyechooo@gmail.com>
2025-06-20 12:24:45 +0530

    Update ping.js

--- src/commands /ping.js
+++ src/commands /ping.js
@@ -5,7 +5,6 @@ export default {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
-
   async execute(interaction) {
     await interaction.reply('🏓 Pong!');
   }
